703378b4002b8901e5eae0104e0e699da4de7679
Nweke Charles <nocrah4@gmail.com>
2022-08-11 14:20:05 +0100

    modified

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ let exitBtn = document.querySelector(".exitBtn");
 exitBtn.addEventListener("click", () => {
     infoPage.classList.toggle("show-links");
 });
+// let pmattLink=document.querySelector("pmatt-link");
+// console.log(window);
